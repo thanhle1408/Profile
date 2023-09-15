37cc836d82b776b235f16bf6e3f5b9828c8e9b32
thanhle1408 <114412004+thanhle1408@users.noreply.github.com>
2023-09-15 14:03:07 +0700

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,23 @@
+const backIcon = require("./imgs/backIcon.png");
+const downloadCvIcon = require("./imgs/downloadCvIcon.png");
+const facebookIcon = require("./imgs/facebookIcon.png");
+const grid = require("./imgs/grid.png");
+const instagramIcon = require("./imgs/instagramIcon.png");
+const scrollDown = require("./imgs/scrollDown.png");
+const trip = require("./imgs/trip.png");
+const videoImg = require("./imgs/videoImg.png");
+const webDesign = require("./imgs/webDesign.png");
+const scrollTop = require("./imgs/scrollTop.gif");
+
+export {
+  backIcon,
+  downloadCvIcon,
+  facebookIcon,
+  grid,
+  instagramIcon,
+  scrollDown,
+  scrollTop,
+  trip,
+  videoImg,
+  webDesign,
+};
